deb7179ae6d45f8a136bdc570df7346162df7bde
Andrej Taneski <andrejtaneski7@gmail.com>
2021-09-10 23:58:00 +0200

    Initial commit

---
+++ meeting-scheduler/src/main/java/mk/ukim/finki/meetingscheduler/MeetingSchedulerApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.meetingscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingSchedulerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetingSchedulerApplication.class, args);
+    }
+
+}
---
+++ meeting-scheduler/src/test/java/mk/ukim/finki/meetingscheduler/MeetingSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.meetingscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingSchedulerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
